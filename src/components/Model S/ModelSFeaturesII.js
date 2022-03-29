40a0f533ad07c9e743edd76029332c2f3fb14aa1
Anurag-1427 <kaushikanurag2001@gmail.com>
2022-03-29 09:54:17 +0530

    Model S FeaturesII fully completed

--- src/components/Model S/ModelSFeaturesII.js
+++ src/components/Model S/ModelSFeaturesII.js
@@ -33,13 +33,22 @@ const ModelSFeaturesII = () => {
             <Inner>
               <p>Plaid</p>
               <h2>Beyond Ludicrous</h2>
+              <span>
+                Model S Plaid has the quickest acceleration of any vehicle in
+                production. Updated battery architecture for all Model S trims
+                enables back-to-back track runs without performance degradation.
+              </span>
               <Button>ORDER NOW</Button>
             </Inner>
           </LeftPart>
           <RightPart>
-              <Under>
-                  <p>Model S Plaid has the quickest acceleration of any vehicle in production. Updated battery architecture for all Model S trims enables back-to-back track runs without performance degradation.</p>
-              </Under>
+            <Under>
+              <p>
+                Model S Plaid has the quickest acceleration of any vehicle in
+                production. Updated battery architecture for all Model S trims
+                enables back-to-back track runs without performance degradation.
+              </p>
+            </Under>
           </RightPart>
         </RestPart>
       </WrapContainer>
@@ -55,12 +64,11 @@ const WrapContainer = styled.div`
   //   background-color: grey;
   display: flex;
   flex-direction: column;
-  
 `;
 const ImgPart = styled.div`
   width: 100vw;
   height: 71vh;
-  border: 1px solid red;
+  // border: 1px solid red;
   display: flex;
   justify-content: center;
   align-items: flex-end;
@@ -70,9 +78,18 @@ const ImgPart = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-image: url("images/MS-Performance-Hero-Desktop.jpg");
+  @media (max-width: 768px)
+  {
+    height: 52vh;
+  }
 `;
 const Wrapper = styled.div`
   display: flex;
+  @media (max-width: 540px)
+  {
+    position: relative;
+    top: 92px;
+  }
 `;
 const One = styled.div`
   h2,
@@ -85,34 +102,61 @@ const One = styled.div`
 const RestPart = styled.div`
   width: 100vw;
   height: 29vh;
-  border: 1px solid blue;
+  // border: 1px solid blue;
   display: flex;
-  @media (max-width: 768px)
-  {
-      flex-direction: column;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    height: 48vh;
   }
 `;
 const LeftPart = styled.div`
-  border: 1px solid green;
+  // border: 1px solid green;
   width: 42%;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  @media (max-width: 768px)
-  {
-      width: 100vw;
+  @media (max-width: 768px) {
+    width: 100vw;
+    height: 48vh;
   }
 `;
 const Inner = styled.div`
-  border: 1px solid black;
+  // border: 1px solid black;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
-  p{
-      font-size: 20px;
-      color: grey;
+  p {
+    font-size: 20px;
+    color: #4e5155;
+  }
+  span{
+    display: none;
+  }
+  h2 {
+    color: #171a20;
+    font-weight: 800;
+  }
+  @media (max-width: 768px)
+  {
+    width: 90vw;
+    span{
+      display: inline;
+      margin-top: 8px;
+      margin-bottom: 8px;
+    }
+    p {
+      font-size: 18px;
+    }
+    h2{
+      margin-top: -15px;
+    }
+    text-align: start;
+    position: relative;
+    left: 2%;
+    bottom: 5%;
+
   }
 `;
 const Button = styled.div`
@@ -120,15 +164,55 @@ const Button = styled.div`
   border: 3px solid black;
   border-radius: 50px;
   padding: 5px;
+  @media (max-width: 768px)
+  {
+    width: 28%;
+    text-align: center;
+    // position: relative;
+    // left: 5%;
+    margin-top: 15px;
+    font-size: 14px;
+    font-weight: 500;
+  }
+  @media (max-width: 530px){
+    width: 100%;
+  }
+  // @media (min-width: 480px)
+  // {
+  //   width: 90%;
+  // }
+  &:hover{
+    background: black;
+    color: white;
+    transition: 0.7s;
+  }
 `;
 const RightPart = styled.div`
-  border: 1px solid blue;
+  // border: 1px solid blue;
   width: 58%;
   display: flex;
   justify-content: flex-start;
   align-items: center;
+  color: #8d8f92;
+  @media (max-width: 768px)
+  {
+    display: none;
+  }
 `;
 const Under = styled.div`
-border: 1px solid blue;
-  
+  // border: 1px solid orange;
+  width: 78%;
+  position: relative;
+  bottom: 5%;
+  left: 5%;
+  text-align: start;
+  p{
+    color: #43464B;
+  }
+  @media (max-width: 768px)
+  {
+    p{
+      display: none;
+    }
+  }
 `;
